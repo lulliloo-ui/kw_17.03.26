25c9a1081516b41e7538940dc5cc451aeb06601c
lulliloo-ui <madieva.tv@edu.spbstu.ru>
2026-03-17 11:17:43 +0300

    Vector top-it

--- main.cpp
+++ main.cpp
@@ -1,2 +1,26 @@
+#include "top-it-vector.hpp"
+#include <iostream>
+#include <iomanip>
+
+
+bool testEmptyVecror()
+{
+  Vector v;
+  return v.isEmpty();
+}
 int main()
-{}
+{
+  using test_t = std::pair< const char*, bool(*)() >;
+  test_t tests[] = {
+    { "Empty vector", testEmptyVecror}
+  };
+  const size_t count = sizeof(tests) / sizeof(test_t);
+  std::cout << std::boolalpha;
+  bool pass = true;
+  for (size_t i = 0; i < count; ++i) {
+    bool res = tests[i].second();
+    std::cout << tests[i].first << ":" << res << "\n";
+    pass = pass && res;
+  }
+  std::cout << "RESULT: " <<pass << "\n";
+}
---
+++ top-it-vector.hpp
@@ -0,0 +1,30 @@
+#ifndef TOP_IT_VECTOR_HPP
+#define TOP_IT_VECTOR_HPP
+#include <cstddef>
+
+namespace topit {
+  template< class T >
+  struct Vector {
+    ~Vector();
+    Vector();
+  private:
+    T * data_;
+    size_t size_, capacity_;
+  };
+}
+
+template< class T >
+topit::Vector< T >::~Vector()
+{
+  
+}
+
+template< class T >
+topit::Vector< T >::Vector() :
+  data_(),
+  size_(),
+  capacity_()
+{
+  
+}
+#endif
