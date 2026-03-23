80ef5fb7dc3241a36c6f81b2ed519a9c349c8ffb
lulliloo-ui <madieva.tv@edu.spbstu.ru>
2026-03-24 00:20:14 +0300

    isEmpty, getSize, getCapacity, pushBack

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 CXXFLAGS += -Wall -Wextra -std=c++14 -MMD
 -include main.d
 main: main.o
-	$(CXX) $^ -0 $@
+	$(CXX) $^ -o $@
--- top-it-vector.hpp
+++ top-it-vector.hpp
@@ -12,9 +12,9 @@ namespace topit {
     Vector& operator=(const Vector&);
     Vector& operator=(Vector&&);
 
-    bool isEmpty() const noexcept;  //
-    size_t getSize() const noexcept;  //
-    size_t getCapacity() const noexcept;  //
+    bool isEmpty() const noexcept;
+    size_t getSize() const noexcept;
+    size_t getCapacity() const noexcept;
 
     void pushBack(const T& v);  //
     void popBack();  //
@@ -43,31 +43,52 @@ topit::Vector< T >::~Vector()
 template< class T >
 bool topit::Vector< T >::isEmpty() const noexcept
 {
-  return !size;
+  return !size_;
 }
 
 template< class T >
 size_t topit::Vector< T >::getSize() const noexcept
 {
-
+  return size_;
 }
 
 template< class T >
 size_t topit::Vector< T >::getCapacity() const noexcept
 {
-
+  return capacity_;
 }
 
 template< class T >
-void topit::Vector< T >::pushBack(const T& v)
+void topit::Vector< T >::pushBack(const T & v)
 {
-
+  if (size_ < capacity_) {
+    data_[size_] = v;
+    size_++;
+  } else {
+    T * new_vec = new T[2 * capacity_ + 1];
+    try {
+      for (size_t i = 0; i < size_; ++i) {
+        new_vec[i] = data_[i];
+      }
+    new_vec[size_] = v;
+    delete [] data_;
+    data_ = new_vec;
+    size_++;
+    capacity_ = capacity_ * 2 + 1;
+    } catch (...) {
+      delete [] new_vec;
+      throw;
+    }
+  }
 }
 
 template< class T >
 void topit::Vector< T >::popBack()
 {
-  
+  if (size_) {
+    data_[size_ - 1].~T();
+    size_--;
+  }
 }
 
 #endif
